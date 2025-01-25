e3b8427b5cbfcebfc54316f64337945d1aa88b98
Kumneger Sintayehu <185130883+KUMNEGER-SINTAYEHU@users.noreply.github.com>
2025-01-25 16:22:55 +0300

    Update ImageUtils.java

--- src/main/java/com/example/braintumorclassifier/util/ImageUtils.java
+++ src/main/java/com/example/braintumorclassifier/util/ImageUtils.java
@@ -14,16 +14,16 @@ public class ImageUtils {
             throw new IllegalArgumentException("Invalid image file.");
         }
 
-        int targetWidth = 224; // Change to the input size of your model
-        int targetHeight = 224; // Change to the input size of your model
+        int targetWidth = 224;
+        int targetHeight = 224;
 
         BufferedImage resizedImg = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
         resizedImg.getGraphics().drawImage(img, 0, 0, targetWidth, targetHeight, null);
 
-        float[][][][] data = new float[1][3][targetHeight][targetWidth]; // Batch size 1, RGB channels
+        float[][][][] data = new float[1][3][targetHeight][targetWidth];
 
-        float[] mean = {0.485f, 0.456f, 0.406f}; // Example mean values for ImageNet
-        float[] std = {0.229f, 0.224f, 0.225f}; // Example std values for ImageNet
+        float[] mean = {0.485f, 0.456f, 0.406f};
+        float[] std = {0.229f, 0.224f, 0.225f};
 
         for (int y = 0; y < targetHeight; y++) {
             for (int x = 0; x < targetWidth; x++) {
